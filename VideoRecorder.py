659e49394e1b0225fa33ddfc3c5d9c863e09f3d7
Sadeq Asadnezhad <sadeg.asadnezhad@gmail.com>
2023-08-06 14:16:40 +0330

    first commit

---
+++ VideoRecorder.py
@@ -0,0 +1,24 @@
+import cv2
+
+import numpy as np
+
+cap= cv2.VideoCapture(0)
+
+fourcc= cv2.VideoWriter_fourcc(*"XVID")
+out= cv2.VideoWriter("myvideo.mp4", fourcc, 20.0 , (640,480))
+
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    
+    if ret ==True:
+        out.write (frame)
+        cv2.imshow("VIDEO", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+    
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
